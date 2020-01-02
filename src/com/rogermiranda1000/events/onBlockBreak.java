57a95e41633af06a35702e5cc2bc7f4a8475e184
Anonymous_Dr <cuentas@anderhurtado.com>
2020-01-02 03:15:57 +0100

    El evento de romper ahora no siempre es cancelado.

--- src/com/rogermiranda1000/events/onBlockBreak.java
+++ src/com/rogermiranda1000/events/onBlockBreak.java
@@ -2,8 +2,10 @@ package com.rogermiranda1000.events;
 
 import com.rogermiranda1000.mineit.MineIt;
 import com.rogermiranda1000.mineit.Mines;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -16,7 +18,7 @@ public class onBlockBreak implements Listener {
             for(String s: m.loc()) {
                 Location loc = e.getBlock().getLocation();
                 if(s.equalsIgnoreCase(loc.getWorld().getName()+","+String.valueOf(loc.getX())+","+String.valueOf(loc.getY())+","+String.valueOf(loc.getZ()))) {
-                    e.setCancelled(true);
+                    //Se aprueba
                     Location l = e.getBlock().getLocation();
                     if(new Location(l.getWorld(), l.getX(), l.getY()+1, l.getZ()).getBlock().getType()==Material.AIR) l.setY(l.getY()+1D);
                     else if(new Location(l.getWorld(), l.getX()+1, l.getY(), l.getZ()).getBlock().getType()==Material.AIR) l.setX(l.getX()+1D);
@@ -25,8 +27,7 @@ public class onBlockBreak implements Listener {
                     else if(new Location(l.getWorld(), l.getX(), l.getY(), l.getZ()-1).getBlock().getType()==Material.AIR) l.setZ(l.getZ()-1D);
                     else if(new Location(l.getWorld(), l.getX(), l.getY()-1, l.getZ()).getBlock().getType()==Material.AIR) l.setY(l.getY()-1D);
 
-                    for(ItemStack item: e.getBlock().getDrops(e.getPlayer().getItemInHand()/*getInventory().getItemInMainHand()*/))
-                        e.getBlock().getWorld().dropItemNaturally(l, item);
+                    //for(ItemStack item: e.getBlock().getDrops(e.getPlayer().getItemInHand()/*getInventory().getItemInMainHand()*/))e.getBlock().getWorld().dropItemNaturally(l, item);
 
                     for(int x = 0; x < m.stages.length; x++) {
                         if(!e.getBlock().getType().toString().equalsIgnoreCase(m.stages[x])) continue;
@@ -37,14 +38,18 @@ public class onBlockBreak implements Listener {
                             m.stageBlocks[x]--;
                             m.stageBlocks[m.stageGo[x-2]]++;
                         }
-                        e.getBlock().setType(Material.getMaterial(m.stages[m.stageGo[x-2]]));
+                        establecer(e.getBlock(),Material.getMaterial(m.stages[m.stageGo[x-2]]));
                         return;
                     }
 
-                    e.getBlock().setType(Material.getMaterial(m.stages[0]));
+                    establecer(e.getBlock(),Material.getMaterial(m.stages[0]));
                     return;
                 }
             }
         }
+    }
+
+    public void establecer(Block b,Material material){
+        Bukkit.getScheduler().runTaskLater(MineIt.instance,()->b.setType(material),1);
     }
 }
