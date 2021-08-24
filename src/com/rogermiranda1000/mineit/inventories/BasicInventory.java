10b622af0d202ed64d1e7b96c27a2c8bd49677c2
roger.miranda <roger.miranda@students.salle.url.edu>
2021-08-24 23:14:57 +0200

    Fix: concurrent modification

--- src/com/rogermiranda1000/mineit/inventories/BasicInventory.java
+++ src/com/rogermiranda1000/mineit/inventories/BasicInventory.java
@@ -1,30 +1,29 @@
 package com.rogermiranda1000.mineit.inventories;
 
-import com.rogermiranda1000.mineit.Mine;
 import com.rogermiranda1000.mineit.MineIt;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.HumanEntity;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 
 public abstract class BasicInventory implements Listener {
     protected Inventory inv;
     private final ArrayList<HumanEntity> playersWithOpenInventory;
+    private boolean swappingInventories;
 
     /**
      * It initializes the players with the current inventory opened list
      */
     public BasicInventory() {
         this.playersWithOpenInventory = new ArrayList<>();
+        this.swappingInventories = false;
     }
 
     /**
@@ -41,7 +40,9 @@ public abstract class BasicInventory implements Listener {
         // run on next tick
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MineIt.instance, ()-> {
             p.openInventory(this.inv);
-            this.playersWithOpenInventory.add(p);
+            synchronized (this.playersWithOpenInventory) {
+                this.playersWithOpenInventory.add(p);
+            }
         }, 1L);
     }
 
@@ -51,21 +52,33 @@ public abstract class BasicInventory implements Listener {
      * Changes all the opened inventories to the new one
      * @param other New inventory
      */
-    protected void newInventory(Inventory other) {
-        for (HumanEntity player : this.playersWithOpenInventory) {
-            player.closeInventory();
+    protected void newInventory(@NotNull Inventory other) {
+        synchronized (this) {
+            this.swappingInventories = true;
 
-            // run on next tick
-            Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MineIt.instance, ()->player.openInventory(other), 1L);
+            for (HumanEntity player : this.playersWithOpenInventory) {
+                player.closeInventory();
+
+                // run on next tick
+                Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MineIt.instance, ()->player.openInventory(other), 1L);
+            }
         }
+
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(MineIt.instance, ()->{
+            synchronized (this) {
+                this.swappingInventories = false;
+            }
+        }, 2L);
     }
 
-    public void closeInventories() {
+    public synchronized void closeInventories() {
         for (HumanEntity player : this.playersWithOpenInventory) player.closeInventory();
     }
 
     @EventHandler
-    public void onInventoryClose(InventoryCloseEvent e) {
+    public synchronized void onInventoryClose(InventoryCloseEvent e) {
+        if (this.swappingInventories) return; // ignore (it will close and open immediately)
+
         if (e.getInventory().equals(this.inv)) this.playersWithOpenInventory.remove(e.getPlayer());
     }
 }
