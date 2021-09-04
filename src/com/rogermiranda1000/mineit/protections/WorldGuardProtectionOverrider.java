ee18bf9612a98d7ce0772af2b7ba41b0f35331ef
rogermiranda1000 <rogermirandaperez@hotmail.com>
2021-09-04 03:34:06 +0200

    Feature: worldguard protection get message

--- src/com/rogermiranda1000/mineit/protections/WorldGuardProtectionOverrider.java
+++ src/com/rogermiranda1000/mineit/protections/WorldGuardProtectionOverrider.java
@@ -5,7 +5,9 @@ import com.sk89q.worldedit.util.Location;
 import com.sk89q.worldguard.LocalPlayer;
 import com.sk89q.worldguard.WorldGuard;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.flags.Flags;
+import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.RegionContainer;
 import com.sk89q.worldguard.protection.regions.RegionQuery;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -15,12 +17,16 @@ public class WorldGuardProtectionOverrider implements ProtectionOverrider {
     public void overrideProtection(BlockBreakEvent event) {
         org.bukkit.Location tmp = event.getBlock().getLocation();
         LocalPlayer localPlayer = WorldGuardPlugin.inst().wrapPlayer(event.getPlayer());
-        Location loc = new Location(BukkitAdapter.adapt(tmp.getWorld()), tmp.getX(), tmp.getY(), tmp.getZ());
+        Location loc = BukkitAdapter.adapt(tmp);
         RegionContainer container = WorldGuard.getInstance().getPlatform().getRegionContainer();
         RegionQuery query = container.createQuery();
 
         if (!query.testState(loc, localPlayer, Flags.BLOCK_BREAK)) {
             // Can't destroy
+            RegionManager manager = container.get( BukkitAdapter.adapt(tmp.getWorld()) );
+            ApplicableRegionSet set = manager.getApplicableRegions(BukkitAdapter.asBlockVector(tmp));
+            String greeting = set.queryValue(localPlayer, Flags.DENY_MESSAGE);
+            System.out.println(greeting);
         }
     }
 }
