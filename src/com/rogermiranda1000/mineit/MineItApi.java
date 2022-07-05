03076e32eb87d44c416bc0c1959c4205e7f99823
roger.miranda <roger.miranda@students.salle.url.edu>
2022-07-05 20:32:11 +0200

    Feature: API

---
+++ src/com/rogermiranda1000/mineit/MineItApi.java
@@ -0,0 +1,40 @@
+package com.rogermiranda1000.mineit;
+
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+
+/**
+ * MineIt API. Use it to access to the plugin's functions.
+ */
+public class MineItApi {
+    private static MineItApi mineItApi = null;
+
+    private MineItApi() { }
+
+    /**
+     * Get the singletone class
+     * @return Object to run all the MineIt API's functions
+     */
+    public MineItApi getInstance() {
+        if (MineItApi.mineItApi == null) MineItApi.mineItApi = new MineItApi();
+        return MineItApi.mineItApi;
+    }
+
+    /**
+     * Get the mine (if any) that a block belongs
+     * @param block Block
+     * @return Block's mine, or null (if any mine)
+     */
+    public Mine getMine(Block block) {
+        return this.getMine(block.getLocation());
+    }
+
+    /**
+     * Get the mine (if any) that a block belongs
+     * @param loc Block location
+     * @return Block's mine, or null (if any mine)
+     */
+    public Mine getMine(Location loc) {
+        return Mine.getMine(loc);
+    }
+}
