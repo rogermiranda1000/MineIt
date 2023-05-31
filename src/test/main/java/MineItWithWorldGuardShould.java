e2a8cf334441603634a1feccd12c814080a300c8
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-05-31 20:59:03 +0200

    Feature: tests

--- src/test/main/java/MineItWithWorldGuardShould.java
+++ src/test/main/java/MineItWithWorldGuardShould.java
@@ -5,20 +5,17 @@ import dev.watchwolf.entities.blocks.Blocks;
 import dev.watchwolf.entities.items.Item;
 import dev.watchwolf.entities.items.ItemType;
 import dev.watchwolf.tester.AbstractTest;
+import dev.watchwolf.tester.ExtendedClientPetition;
 import dev.watchwolf.tester.TesterConnector;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith(MineItWithWorldGuardShould.class)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MineItWithWorldGuardShould extends AbstractTest {
     /* WORLD INFORMATION ZONE */
     private static final Position MINE_BLOCK_INSIDE_WG_REGION = new Position("world", 999.5, 101.5, 998.5);
@@ -49,28 +46,85 @@ public class MineItWithWorldGuardShould extends AbstractTest {
         server.server.opPlayer(username); // we need the user to be admin if we want to add&remove mines
         server.getClientPetition(0).runCommand("mineit tool");
 
-        // TODO create WG region
+        try {
+            // some tests report that the tool wasn't found (maybe this solves it?)
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // we need a mine in MINE_BLOCK_INSIDE_WG_REGION and MINE_BLOCK_OUTSIDE_WG_REGION
+        try {
+            new MineItShould().createMineWithLeftClick(server);
+            server.runCommand("mineit start " + MineItShould.CREATED_MINE_IN_TESTS_NAME);
+            // set the blocks to the max. stage, so they don't change forwards
+            server.setBlock(MINE_BLOCK_INSIDE_WG_REGION, Blocks.DIAMOND_ORE);
+            server.setBlock(MINE_BLOCK_OUTSIDE_WG_REGION, Blocks.DIAMOND_ORE);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+        }
+
+        // create WG region
+        server.createRegion("mine", MINE_BLOCK_INSIDE_WG_REGION, BLOCK_INSIDE_WG_REGION);
+
+        server.runCommand("deop " + username); // we need a normal user to test the WG region
     }
 
     private static void equipPickaxe(ClientPetition client) throws IOException {
         client.equipItemInHand(new Item(ItemType.DIAMOND_PICKAXE));
     }
 
-    private static void equipTool(ClientPetition client) throws IOException {
-        client.equipItemInHand(new Item(MineItWithWorldGuardShould.MINEIT_TOOL_ITEMTYPE));
-    }
-
     /* TESTS ZONE */
 
-    /**
-     * In the beforeAll we've runned `/mineit tool`, so the user should have the tool
-     */
-    @Order(1)
     @ParameterizedTest
     @ArgumentsSource(MineItWithWorldGuardShould.class)
-    public void addMineItTool(TesterConnector connector) throws Exception {
-        new MineItShould().createMineWithLeftClick(connector);
+    public void allowMineInsideWorldGuardRegion(TesterConnector connector) throws Exception {
+        ExtendedClientPetition userPetition = connector.getClientPetition(0);
 
-        assertTrue(connector.runCommand("mineit list").contains(MineItShould.CREATED_MINE_IN_TESTS_NAME));
+        MineItWithWorldGuardShould.equipPickaxe(userPetition);
+
+        int tries = 6; // there's some chance that the stage goes immediately back to stone; we'll try a few times
+        Block got = null;
+        while (tries > 0 && !Blocks.STONE.equals(got)) {
+            userPetition.breakBlock(MINE_BLOCK_INSIDE_WG_REGION);
+            got = connector.server.getBlock(MINE_BLOCK_INSIDE_WG_REGION);
+            tries--;
+        }
+
+        assertEquals(Blocks.STONE, got);
+    }
+
+    @ParameterizedTest
+    @ArgumentsSource(MineItWithWorldGuardShould.class)
+    public void allowMineOutsideWorldGuardRegion(TesterConnector connector) throws Exception {
+        ExtendedClientPetition userPetition = connector.getClientPetition(0);
+
+        MineItWithWorldGuardShould.equipPickaxe(userPetition);
+
+        int tries = 6; // there's some chance that the stage goes immediately back to stone; we'll try a few times
+        Block got = null;
+        while (tries > 0 && !Blocks.STONE.equals(got)) {
+            userPetition.breakBlock(MINE_BLOCK_OUTSIDE_WG_REGION);
+            got = connector.server.getBlock(MINE_BLOCK_OUTSIDE_WG_REGION);
+            tries--;
+        }
+
+        assertEquals(Blocks.STONE, got);
+    }
+
+    /**
+     * If there's no mine, then WG should protect that area
+     */
+    @ParameterizedTest
+    @ArgumentsSource(MineItWithWorldGuardShould.class)
+    public void denyBreakInsideWorldGuardRegion(TesterConnector connector) throws Exception {
+        ExtendedClientPetition userPetition = connector.getClientPetition(0);
+
+        Block blockInWorld = connector.getBlock(BLOCK_INSIDE_WG_REGION);
+
+        MineItWithWorldGuardShould.equipPickaxe(userPetition);
+        userPetition.breakBlock(BLOCK_INSIDE_WG_REGION);
+
+        assertEquals(blockInWorld, connector.getBlock(BLOCK_INSIDE_WG_REGION));
     }
 }
